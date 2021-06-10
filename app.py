f3702adf8bf4b126fb5406bab5d82e7c9e039719
VanWyknBake <36945528+VanWyknBake@users.noreply.github.com>
2021-06-10 19:58:54 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ def note():
     name = input("Title: ")
     note = input("Notes: ")
     
-    rec = name.capitalize() + '\n\n'+note.title()
+    rec = name.uppercase() + '\n\n'+note.title()
 
     with open(name, 'w') as file:
         content = file.write(rec)
